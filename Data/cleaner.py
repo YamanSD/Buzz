0e299620caf2a7224c28cc79e24893606e332666
YamanSD <mysd.real@gmail.com>
2024-04-25 15:04:48 +0300

    Fixed the implementation of clean_headlines

--- Data/cleaner.py
+++ Data/cleaner.py
@@ -1,7 +1,5 @@
 from os import path
-from pandas import DataFrame, Series, Timedelta, to_datetime, \
-    read_parquet
-from pandas.core.dtypes.common import is_numeric_dtype
+from pandas import DataFrame, to_datetime, read_parquet
 
 from .io import load_headlines, save_parquet, dir_path
 
@@ -16,57 +14,21 @@ def clean_headlines() -> DataFrame:
 
     """
     df: DataFrame = load_headlines()
-    stats: dict = {}
 
-    # TODO implement
-
-    # Initial data size
-    init_size: int = len(df.index)
-
-    stats['init_size'] = init_size
-    stats['init_shape'] = df.shape
+    # Drop missing values
+    df.drop(inplace=True)
 
     # Timestamp column name
-    ts: str = 'timestamp'
-
-    # Clean the data
-    stats['missing_vals'] = df.isna().sum()
-
-    # Remove negative values
-    has_neg: bool = False
-
-    for column in df.columns:
-        if not is_numeric_dtype(df[column]):
-            continue
-
-        if any(df[column] < 0):
-            has_neg = True
-            df = df[df[column] >= 0]
-
-    stats['has_invalid'] = has_neg
+    ts: str = 'publish_date'
 
     # Convert string timestamps to DateTime
-    df[ts] = to_datetime(df[ts])
-
-    prev_sz: int = len(df.index)
+    df[ts] = to_datetime(df[ts], format='%y%m%d')
 
     # Remove duplicated timestamps
     df = df.drop_duplicates(ts, keep="last")
 
-    # Number of duplicates
-    dup_sz: int = prev_sz - len(df.index)
-
-    stats['dup_timestamps'] = dup_sz
-
     # Sort the entries by their timestamp
     df.sort_values(ts, ascending=True, inplace=True)
-
-    ts_diff: Series | DataFrame = df[ts].diff()
-    invalid_ts: int = (ts_diff != Timedelta(minutes=1)).sum()
-
-    stats['invalid_timestamps'] = invalid_ts
-    stats['max_timestamp_dif'] = ts_diff.max()
-    stats['bad_perc'] = ((invalid_ts + dup_sz) / init_size) * 100
 
     # Set the timestamp column as the index
     df.set_index(ts, inplace=True)
